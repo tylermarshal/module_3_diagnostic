cb3e0a670a16ecea2f75b2536b1261adff3e1e58
TMad <madsen.tyler@gmail.com>
2018-02-07 10:55:33 -0700

    refactor NrelService into couple methods

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -7,11 +7,14 @@ class NrelService
   end
 
   def request
-    request = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=ELEC,LPG&location=#{location}&limit=10&radius=6.0")
-    response = JSON.parse(request.body, symbolize_names: true)
-    @stations = response[:fuel_stations].map do |station|
+    get_nrel[:fuel_stations].map do |station|
       Station.new(station)
     end
+  end
+
+  def get_nrel
+    request = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=ELEC,LPG&location=#{location}&limit=10&radius=6.0")
+    JSON.parse(request.body, symbolize_names: true)
   end
 
 end
