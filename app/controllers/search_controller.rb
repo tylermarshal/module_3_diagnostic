72cefc1ee9c420650d46723d92d89bbf9b5c32fb
TMad <madsen.tyler@gmail.com>
2018-02-07 10:30:45 -0700

    move station PORO to services folder

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,17 +9,3 @@ class SearchController < ApplicationController
   end
 
 end
-
-class Station
-
-  attr_reader :name, :address, :fuel_type, :distance, :access_times
-
-  def initialize(response)
-    @name = response[:station_name]
-    @address = response[:street_address]
-    @fuel_type = response[:fuel_type_code]
-    @distance = "Within 6 Miles"
-    @access_times = response[:access_days_time]
-  end
-
-end
