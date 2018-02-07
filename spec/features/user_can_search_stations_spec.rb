27872a57211dbb68b1881b3118869f268828924c
TMad <madsen.tyler@gmail.com>
2018-02-07 10:30:27 -0700

    remove commented out user story from test

--- spec/features/user_can_search_stations_spec.rb
+++ spec/features/user_can_search_stations_spec.rb
@@ -30,13 +30,3 @@ describe 'a user visits the homepage' do
     end
   end
 end
-
-
-# As a user
-# When I visit "/"
-# And I fill in the search form with 80203
-# And I click "Locate"
-# Then I should be on page "/search" with parameters visible in the url
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-# And the stations should be limited to Electric and Propane
-# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
