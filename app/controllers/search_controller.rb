a472be1c205b38a83ff6227649a7107bc42ce5e2
TMad <madsen.tyler@gmail.com>
2018-02-07 10:05:40 -0700

    add faraday request to search index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
 
   def index
-
+    request = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=ELEC,LPG&zip=(#{params[:q].to_i})&limit=10&format=JSON&radius=6.0")
+    response = JSON.parse(request.body)
+    @stations = Station.new(response)
   end
 
 end
