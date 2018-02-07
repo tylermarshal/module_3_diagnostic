bc2cb2919c58df83eb220bc6000796398b6e1055
TMad <madsen.tyler@gmail.com>
2018-02-07 10:17:28 -0700

    build station class in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,23 @@ class SearchController < ApplicationController
   def index
     request = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=ELEC,LPG&zip=(#{params[:q].to_i})&limit=10&format=JSON&radius=6.0")
     response = JSON.parse(request.body)
-    @stations = Station.new(response)
+    @stations = response[:fuel_stations].each do |station|
+      Station.new(station)
+    end
+  end
+
+end
+
+class Station
+
+  attr_reader :name, :address, :fuel_type, :distance, :access_times
+
+  def initialize(response)
+    @name = response[:station_name]
+    @address = response[:street_address]
+    @fuel_type = response[:fuel_type_code]
+    @distance = "Within 6 Miles"
+    @access_times = response[:access_days_time]
   end
 
 end
