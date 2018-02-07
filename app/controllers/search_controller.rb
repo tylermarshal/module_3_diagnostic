64e4c5e44672bec9bacb243ca091bb5075bac76e
TMad <madsen.tyler@gmail.com>
2018-02-07 10:52:02 -0700

    refactor controller to use NrelService PORO

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    request = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=ELEC,LPG&location=#{params[:q].to_i}&limit=10&radius=6.0")
-    response = JSON.parse(request.body, symbolize_names: true)
-    @stations = response[:fuel_stations].map do |station|
-      Station.new(station)
-    end
+    @stations = NrelService.new(params[:q].to_i).request
   end
 
 end
