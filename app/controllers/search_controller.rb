5a47fc36b7c8c453465c21f8586622a42ed192d6
TMad <madsen.tyler@gmail.com>
2018-02-07 10:25:57 -0700

    @stations now maps instead of using each"

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
 
   def index
-    request = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=ELEC,LPG&zip=(#{params[:q].to_i})&limit=10&format=JSON&radius=6.0")
-    response = JSON.parse(request.body)
-    @stations = response[:fuel_stations].each do |station|
+    request = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=ELEC,LPG&zip=#{params[:q].to_i}&limit=10&format=JSON&radius=6.0")
+    response = JSON.parse(request.body, symbolize_names: true)
+    @stations = response[:fuel_stations].map do |station|
       Station.new(station)
     end
   end
